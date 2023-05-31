9a25a2a67286f6e1de04edcf9e254e2e1a53e1ba
DenisS <semenovdenis3201@gmail.com>
2023-05-31 11:59:50 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 42;
+            int b = 24;
+            int x = TestMethod0(a);
+            Assert.AreEqual(b, x);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 0;
+            int b = 0;
+            int x = TestMethod0(a);
+            Assert.AreEqual(b, x);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 5;
+            int b = 5;
+            int x = TestMethod0(a);
+            Assert.AreEqual(b, x);
+        }
+        private int TestMethod0(int x)
+        {
+            if (x < 10 || x > 99)
+            {
+                return x;
+            }
+            int a = x / 10;
+            int b = x % 10;
+            return b * 10 + a;
+        }
+    }
+}
---
+++ WpfApp32/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp32
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static int f(int x)
+        {
+            int a = x / 10;
+            int b = x % 10;
+            return b * 10 + a;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(Boxx.Text);
+            int x1 = f(x);
+            txtOtv.Text = $" {x1}";
+        }
+    }
+}
